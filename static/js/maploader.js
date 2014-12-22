cb6f876628ec2503566f1aba804ee41972a7fa77
Genji Noguchi <genji.noguchi@gmail.com>
2014-12-22 10:31:48 -0500

    Markers are draggable.

--- static/js/maploader.js
+++ static/js/maploader.js
@@ -1,6 +1,34 @@
 var marker;
+var defaultMarker = {
+	animation: google.maps.Animation.DROP,
+	draggable: true
+}
+
 var latlng = {};
-var map
+var map;
+
+function setMarker(marker){
+	return function(latlng){
+		var pos = new google.maps.LatLng(latlng.latitude, latlng.longitude);
+		marker = new google.maps.Marker({
+			map: map,
+		    position: pos,
+		    animation: defaultMarker.animation,
+		    draggable: defaultMarker.draggable
+		});
+		map.panTo(marker.getPosition());
+		google.maps.event.addListener(marker, 'dragend', function(){
+   			map.panTo(marker.getPosition());
+   		});
+	}
+};
+function toggleBounce() {
+  	if (marker.getAnimation() != null) {
+    	marker.setAnimation(null);
+  	} else {
+    	marker.setAnimation(google.maps.Animation.BOUNCE);
+  	}
+}
 
 function initialize() {
     var mapOptions = {
@@ -13,59 +41,45 @@ function initialize() {
 
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    marker = new google.maps.Marker({
-		map: map,
-		draggable: true
-	})
-
-    latlong = loadLocation(map);
+    latlong = loadLocation(setMarker(marker), function(error){	
+	    switch(error.code) {
+		    case error.PERMISSION_DENIED:
+				console.log("User denied permission.");
+				break;
+		    case error.POSITION_UNAVAILABLE:
+				console.log("Geolocation/GPS unavailable.");
+				break;
+		    case error.TIMEOUT:
+				console.log("Geolocation request timed out.");
+				break;
+		    case error.UNKNOWN_ERROR:
+				console.log("Something wack happened.");
+				break;
+    	}
+    });
 
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
-function loadLocation (){
+
+
+
+//Callbacks
+function loadLocation (callback, error){
 
 	function getLocation(pos){
 		latlng["latitude"] = pos.coords.latitude;
 		latlng["longitude"] = pos.coords.longitude;
-		setMarker();
+		callback(latlng);
 	}
 
 	var load = function() {
 		if (navigator.geolocation) {
 			console.log("Loading location now....");
-		    navigator.geolocation.getCurrentPosition(getLocation, function(error){	
-				    switch(error.code) {
-					    case error.PERMISSION_DENIED:
-							console.log("User denied permission.");
-							break;
-					    case error.POSITION_UNAVAILABLE:
-							console.log("Geolocation/GPS unavailable.");
-							break;
-					    case error.TIMEOUT:
-							console.log("Geolocation request timed out.");
-							break;
-					    case error.UNKNOWN_ERROR:
-							console.log("Something wack happened.");
-							break;
-			    }
-
-			});
+		    navigator.geolocation.getCurrentPosition(getLocation, error);
 		}
 	}();
-
-	function setMarker(){
-		console.log(latlng);
-		var pos = new google.maps.LatLng(latlng.latitude, latlng.longitude);
-		marker = new google.maps.Marker({
-			map: map,
-		    position: pos,
-		    animation: google.maps.Animation.DROP,
-		    draggable: true
-		});
-		map.panTo(marker.getPosition());
-	};
 
 };
