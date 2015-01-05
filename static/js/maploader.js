cbd6bc4c8ea53592db4767cb8ae50d097aa272f1
Genji Noguchi <genji.noguchi@gmail.com>
2015-01-05 12:15:55 -0500

    Schedule

--- static/js/maploader.js
+++ static/js/maploader.js
@@ -4,31 +4,9 @@ var defaultMarker = {
 	draggable: true
 }
 
-var latlng = {};
+var currentLocation={};
 var map;
 
-function setMarker(marker){
-	return function(latlng){
-		var pos = new google.maps.LatLng(latlng.latitude, latlng.longitude);
-		marker = new google.maps.Marker({
-			map: map,
-		    position: pos,
-		    animation: defaultMarker.animation,
-		    draggable: defaultMarker.draggable
-		});
-		map.panTo(marker.getPosition());
-		google.maps.event.addListener(marker, 'dragend', function(){
-   			map.panTo(marker.getPosition());
-   		});
-	}
-};
-function toggleBounce() {
-  	if (marker.getAnimation() != null) {
-    	marker.setAnimation(null);
-  	} else {
-    	marker.setAnimation(google.maps.Animation.BOUNCE);
-  	}
-}
 
 function initialize() {
     var mapOptions = {
@@ -41,7 +19,7 @@ function initialize() {
 
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    latlong = loadLocation(setMarker(marker), function(error){	
+    currentLocation = loadLocation(setMarker(marker), function(error){	
 	    switch(error.code) {
 		    case error.PERMISSION_DENIED:
 				console.log("User denied permission.");
@@ -65,9 +43,50 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
+function setMarker(marker){
+	/*
+	var contentString = "<div class=\"align-center\"> <button class=\"align-center\" type=\"button\">Drop a Crumb</button> </div>"
+	var infowindow = new google.maps.InfoWindow({
+    	content: contentString
+  	});
+	*/
+
+	return function(latlng){
+		var pos = new google.maps.LatLng(latlng.latitude, latlng.longitude);
+		marker = new google.maps.Marker({
+			map: map,
+		    position: pos,
+		    animation: defaultMarker.animation,
+		    draggable: defaultMarker.draggable
+		});
+		map.panTo(marker.getPosition());
+		
+		google.maps.event.addListener(marker, 'clicked', function(){
+			console.log("hello there");
+			toggleBounce(marker);
+			//infowindow.open(map, marker);
+		})
+		google.maps.event.addListener(marker, 'dragend', function(){
+   			map.panTo(marker.getPosition());
+   		});
+	}
+};
+function toggleBounce(marker) {
+  	if (marker.getAnimation() != null) {
+    	marker.setAnimation(null);
+  	} else {
+    	marker.setAnimation(google.maps.Animation.BOUNCE);
+  	}
+}
+
+
+
+
+
 
 //Callbacks
 function loadLocation (callback, error){
+	latlng = currentLocation;
 
 	function getLocation(pos){
 		latlng["latitude"] = pos.coords.latitude;
