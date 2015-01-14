e6d5eccd73ed6d46e78f925f4c18cdee6d0274c8
Justin Kim <yulli@yulli.org>
2015-01-14 13:43:18 -0500

    Establish User-Crumb relationship, add User.from_omniauth

--- app/models/crumb.rb
+++ app/models/crumb.rb
@@ -1,2 +1,3 @@
 class Crumb < ActiveRecord::Base
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  has_many :crumbs
+
+  validates_presence_of :provider, :uid, :name, :oauth_token, :oauth_expires_at
+  validates_uniqueness_of :uid
+
+  def self.from_omniauth(auth_hash)
+    where(auth_hash.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth_hash.provider
+      user.uid = auth_hash.uid
+      user.name = auth_hash.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
