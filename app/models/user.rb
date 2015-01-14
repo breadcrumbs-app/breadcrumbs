e5eebd61a77908498584d9e34462bf8b1d47ef5d
Justin Kim <yulli@yulli.org>
2015-01-14 14:47:42 -0500

    Avoid using slice in order to satisfy strong params

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ActiveRecord::Base
   validates_presence_of :provider, :uid, :name, :oauth_token, :oauth_expires_at
   validates_uniqueness_of :uid
 
-  def self.from_omniauth(auth_hash)
-    where(auth_hash.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth_hash.provider
-      user.uid = auth_hash.uid
-      user.name = auth_hash.info.name
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      user.save!
+      user.save
     end
   end
 end
