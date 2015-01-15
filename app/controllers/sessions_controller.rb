e6751a4b05f2c82401b50065125d5e1f02da9a81
Justin Kim <yulli@yulli.org>
2015-01-15 11:21:23 -0500

    Fix typo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
+    user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = user.id
     redirect_to root_url
   end
 
   def destroy
     reset_session
-    redirect_url root_url, notice: 'Signed out!'
+    redirect_to root_url, notice: 'Signed out!'
   end
 end
