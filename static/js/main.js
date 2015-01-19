1d207880f644a2d23e7c30d739fd6630585139d3
Genji Noguchi <genji.noguchi@gmail.com>
2015-01-19 14:33:09 -0500

    Things

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,49 @@
+/* /////////////////////////////////////////////////////////////////////////////////
+ * /////////////////////////////////////////////////////////////////////////////////
+ * 
+ * __________                            .___                        ___.           
+ * \______   \_______   ____ _____     __| _/___________ __ __  _____\_ |__   ______
+ *  |    |  _/\_  __ \_/ __ \\__  \   / __ |/ ___\_  __ \  |  \/     \| __ \ /  ___/
+ *  |    |   \ |  | \/\  ___/ / __ \_/ /_/ \  \___|  | \/  |  /  Y Y  \ \_\ \\___ \ 
+ *  |______  / |__|    \___  >____  /\____ |\___  >__|  |____/|__|_|  /___  /____  >
+ *         \/              \/     \/      \/    \/                  \/    \/     \/ 
+ *
+ * /////////////////////////////////////////////////////////////////////////////////
+ * /////////////////////////////////////////////////////////////////////////////////
+ * //////////////////////Copyright 2015 Team Breadcrumbs////////////////////////////
+ * /////////////////////////////////////////////////////////////////////////////////
+ * ////////////////Genji Noguchi, Justin Kim, Eric Chen/////////////////////////////
+ * /////////////////////////////////////////////////////////////////////////////////
+ * /////////////////////////////////////////////////////////////////////////////////
+ */
 
-//Constants
+/* Globals */
 var marker;
+var currentLocation={};
+var map;
+
+/* Constants */
 var defaultMarker = {
 	animation: google.maps.Animation.DROP,
 	draggable: true
 };
 
-var currentLocation={};
-var map;
 
 $(document).ready( function(){
 	google.maps.event.addDomListener(window, 'load', initialize);
 
 	/*
 	 *  Testing to see if the function works.
-	 *  I'm delaying by 5 seconds in order to get the current location to load first.
+	 *  I'm delaying by x seconds in order to get the current location to load first.
 	 */
 	setTimeout(function(){
 		createCrumb({
 			latitude: currentLocation['latitude'],
-			longitude: currentLocationp['longitude'],
+			longitude: currentLocation['longitude'],
 			timestamp: Date.now(),
-			session: getSession();
-		}
+			session: getSession()
+		})
+	}, 10000);
 
 
 });
