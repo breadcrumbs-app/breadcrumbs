3fe44cc45ce0d18b1de70e779216b1b887d3e8f0
Genji Noguchi <genji.noguchi@gmail.com>
2015-01-19 18:26:47 -0500

    Session filtering using regex.

--- static/js/sessionhandler.js
+++ static/js/sessionhandler.js
@@ -1,13 +1,22 @@
 /*
  * Manipulation with the web app's cookies.
  * The javascript code isn't doing anything to handle the cookies; rather, it's simply retrieving the encrypted cookie information.
- * Our app uses Python Flask's session manager to manage our cookies.
+ * Our app uses the Rails session manager to manage our session.
  *
- *
+ * NOTE
+ * For the future, change this to use an XMLHTTPRequest to request cookies in order to maintain the security of cookies using HTTPOnly.
  */
 
+/*
+ * Gets the encrypted session blob
+ * The name of the cookie should be _breadcrumbs_session.
+ */
 function getSession() {
-	
+	var session="";
 
-	return "";
+	/* retrieve the cookie using regex. Regex still needs work */
+	var test = /^_breadcrumbs_session=\w+$/;
+	session = test.exec(document.cookie);
+
+	return session;
 }
